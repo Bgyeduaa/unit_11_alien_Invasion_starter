d66b9767e46e961f3168c168d27139fe550f9e27
Bgyeduaa <berlindagyeduaa22@gmail.com>
2025-07-20 16:01:34 -0400

    Unit11 Video5 Added events for ship moving left and right, built boundaries

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ class Settings:
 
     def __init__(self) -> None:
         self.name: str = "Alien Invasion"
-        self.screen_w = 800
+        self.screen_w = 1200
         self.screen_h = 600
         self.FPS = 60
         self.bg_file = Path.cwd() / "Assets"/ "images" / "Starbasesnow.png"
@@ -11,5 +11,6 @@ class Settings:
         self.ship_file = Path.cwd() / "Assets" / "images" / "ship2(no bg).png"
         self.ship_w = 40
         self.ship_h = 60
+        self.ship_speed = 5
 
 
