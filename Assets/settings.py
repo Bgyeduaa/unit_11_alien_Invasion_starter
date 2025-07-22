bf2ab0eeed816c5f8f53c3e0bf8a27981690c4c0
Bgyeduaa <berlindagyeduaa22@gmail.com>
2025-07-22 11:07:54 -0400

    U12 V3 modified alien_fleet to create all the rows

--- Assets/settings.py
+++ Assets/settings.py
@@ -3,7 +3,7 @@ class Settings:
 
     def __init__(self) -> None:
         self.name: str = "Alien Invasion"
-        self.screen_w = 800
+        self.screen_w = 1200
         self.screen_h = 600
         self.FPS = 60
         self.bg_file = Path.cwd() / "Assets"/ "images" / "Starbasesnow.png"
