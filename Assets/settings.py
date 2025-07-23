59790e7ac945c880ca436dbc3a7afe668c0b71c4
Bgyeduaa <berlindagyeduaa22@gmail.com>
2025-07-23 05:33:45 -0400

    Unit12  Lab12

--- Assets/settings.py
+++ Assets/settings.py
@@ -14,7 +14,7 @@ class Settings:
         self.ship_speed = 5
 
         self.bullet_file = Path.cwd() / "Assets" / "images" / "laserBlast-removebg.png"
-        self.laser_sound = Path.cwd() / "Assets" / "sound" / "laser.mp3"
+        self.laser_sound = Path.cwd() / "Assets" / "sound" / "zapsplat.mp3"
         self.bullet_speed = 7
         self.bullet_w = 25
         self.bullet_h = 80
